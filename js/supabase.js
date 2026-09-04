ff96ec5fd27a99490dab0c849a619fb58a51cb07
MegumiNoHikari-Kun <khoirulnr2501@gmail.com>
2026-09-05 03:59:04 +0700

    Clean up comments in supabase.js
    Removed commented sections related to Supabase configuration and authentication.

--- js/supabase.js
+++ js/supabase.js
@@ -1,28 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-/*
-|--------------------------------------------------------------------------
-| SUPABASE CONFIG
-|--------------------------------------------------------------------------
-| Ambil dari:
-| Supabase Dashboard
-| → Project Settings
-| → API
-|
-| Jangan gunakan service_role key di frontend.
-|--------------------------------------------------------------------------
-*/
 
 const SUPABASE_URL = 'https://wnftkpopbgtupdxruqsj.supabase.co'; // Ganti dengan URL Supabase Anda
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduZnRrcG9wYmd0dXBkeHJ1cXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNDk4NzIsImV4cCI6MjA5ODkyNTg3Mn0.HWEY7E3FUZQ6bPOC-0XhXxUZF6119N5POhrVF934RwE'; // Ganti dengan Anon/Public Key Anda
-
-
-
-/*
-|--------------------------------------------------------------------------
-| CREATE SUPABASE CLIENT
-|--------------------------------------------------------------------------
-*/
 
 export const supabase = createClient(
     SUPABASE_URL,
@@ -37,26 +17,9 @@ export const supabase = createClient(
     }
 );
 
-
-/*
-|--------------------------------------------------------------------------
-| AUTH READY
-|--------------------------------------------------------------------------
-| Digunakan oleh halaman seperti:
-| produk.html
-| admin.html
-| kasir.html
-| dll.
-|
-| Tidak melakukan anonymous login.
-| Session hanya berasal dari login admin yang sebenarnya.
-|--------------------------------------------------------------------------
-*/
-
 export const authReady = (async () => {
 
     try {
-
         const {
             data,
             error
@@ -74,10 +37,10 @@ export const authReady = (async () => {
 
         const session = data?.session || null;
 
-        console.log(
-            'Supabase session:',
-            session
-        );
+        // console.log(
+        //     'Supabase session:',
+        //     session
+        // );
 
         return session;
 
@@ -105,11 +68,11 @@ export const authReady = (async () => {
 supabase.auth.onAuthStateChange(
     (event, session) => {
 
-        console.log(
-            'Supabase Auth State:',
-            event,
-            session
-        );
+        // console.log(
+        //     'Supabase Auth State:',
+        //     event,
+        //     session
+        // );
 
     }
 );
